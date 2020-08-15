48b80390d6c0dcec2629d72d8f5fd02e363785d3
54mpenguin <54mpenguin@gmail.com>
2020-08-15 20:20:10 +1200

    Added redeeming lifeorbs if player's lives are low

--- src/com/i54m/hardcorelives/listeners/PlayerDeath.java
+++ src/com/i54m/hardcorelives/listeners/PlayerDeath.java
@@ -1,6 +1,7 @@
 package com.i54m.hardcorelives.listeners;
 
 import com.i54m.hardcorelives.Main;
+import com.i54m.hardcorelives.managers.PlayerDataManager;
 import org.bukkit.ChatColor;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
@@ -9,17 +10,29 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
 
 public class PlayerDeath implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerDeath(PlayerDeathEvent event) {
+        event.setKeepInventory(true);
         Player player = event.getEntity();
         player.spawnParticle(Particle.PORTAL, player.getLocation(), Main.getInstance().getDeathParticleAmount(), 0.5, 0.5, 0.5);
         player.playSound(player.getLocation(), Sound.ENTITY_WITHER_DEATH, 1, 1);
         player.sendMessage(ChatColor.RED + "You have died and lost a life!");
-//        player.spigot().respawn();//force respawn to avoid bugs
-        //drop soul gem?
+        ItemStack lifeItem = Main.getInstance().getLifeItem();
+        PlayerDataManager playerDataManager = PlayerDataManager.getINSTANCE();
+        int lives = playerDataManager.getPlayerData(player, false).getInt("lives");
+        if (player.getInventory().contains(lifeItem) && lives <= 0) {
+            HashMap<Integer, ? extends ItemStack> lifeItems = player.getInventory().all(lifeItem);
+            lives = lives+lifeItems.values().size();
+            playerDataManager.getPlayerData(player, false).set("lives", lives);
+            player.sendMessage(ChatColor.GREEN + "Your inventory contained life orbs, they were automatically redeemed for you on death due to your low amount of lives!");
+            playerDataManager.savePlayerData(player.getUniqueId());
+        }
     }
 
 }
